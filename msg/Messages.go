36ddc407b45cba36aabe4c46f789d3054bc49972
Raffaele Meyer <rafmeyer91@gmail.com>
2016-06-08 15:55:40 +0200

    Added messages

--- msg/Messages.go
+++ msg/Messages.go
@@ -16,6 +16,7 @@ const (
 	CreateNwType byte = iota
 	JoinNwType
 	DisconnectNw
+	NotifyNetworkJoined
 	NotifyClientDisconect
 	NotifyClientJoin
 )
@@ -46,6 +47,27 @@ type JoinNetworkCommand struct{
 	Peername string
 	Peerport string
 }
+
+type DisconnectNetworkCommand struct {
+	VirtualIPAddress net.IP
+}
+
+type ClientDisconectNotification struct {
+	VirtualIPAddress net.IP
+}
+type ClientJoinNotification struct {
+	VirtualIPAddress net.IP
+	RemoteAddress net.UDPAddr
+	Peername string
+}
+type ClientDisconnectNotification struct {
+	VirtualIPAddress net.IP
+}
+type NetworkJoinNotification struct {
+	VirtualIPAddress net.IP
+	Peers []*Peer
+}
+
 
 type MessageHandler interface {
 	OnCommand(commandType byte, message interface{})
@@ -97,15 +119,55 @@ func ReadMessage(rd io.Reader, handler MessageHandler) (error) {
 	handler.OnCommand(cmdType, msg)
 	return nil
 }
-
+func EncodeNetworkJoinNotification(data *NetworkJoinNotification) []byte {
+	header := []byte{Version, 0, 0, 0, 0, NotifyNetworkJoined}
+	p, err := json.Marshal(&data)
+	if err != nil {
+		panic(err)
+	}
+	p = append(header, p...)
+	binary.BigEndian.PutUint32(p[1:5], uint32(len(p)))
+	return p
+}
+func EncodeClientJoinNotification(data *ClientJoinNotification) []byte {
+	header := []byte{Version, 0, 0, 0, 0, NotifyClientJoin}
+	p, err := json.Marshal(&data)
+	if err != nil {
+		panic(err)
+	}
+	p = append(header, p...)
+	binary.BigEndian.PutUint32(p[1:5], uint32(len(p)))
+	return p
+}
+func EncodeClientDisconnectNotification(data *ClientDisconnectNotification) []byte {
+	header := []byte{Version, 0, 0, 0, 0, NotifyClientDisconect}
+	p, err := json.Marshal(&data)
+	if err != nil {
+		panic(err)
+	}
+	p = append(header, p...)
+	binary.BigEndian.PutUint32(p[1:5], uint32(len(p)))
+	return p
+}
+func EncodeDisconnectNetworkCommand(data *DisconnectNetworkCommand) []byte {
+	header := []byte{Version, 0, 0, 0, 0, DisconnectNw}
+	p, err := json.Marshal(&data)
+	if err != nil {
+		panic(err)
+	}
+	p = append(header, p...)
+	binary.BigEndian.PutUint32(p[1:5], uint32(len(p)))
+	return p
+}
 func EncodeCreateNetworkCommand(data *CreateNetworkCommand) []byte {
 	header := []byte{Version, 0, 0, 0, 0, CreateNwType}
 	p, err := json.Marshal(&data)
 	if err != nil {
 		panic(err)
 	}
-	binary.BigEndian.PutUint32(header[1:5], uint32(len(p)))
-	return append(header, p...)
+	p = append(header, p...)
+	binary.BigEndian.PutUint32(p[1:5], uint32(len(p)))
+	return p
 }
 func EncodeJoinNetworkCommand(data *JoinNetworkCommand) []byte{
 	header := []byte{Version, 0, 0, 0, 0, JoinNwType}
@@ -113,17 +175,38 @@ func EncodeJoinNetworkCommand(data *JoinNetworkCommand) []byte{
 	if err != nil {
 		panic(err)
 	}
-	binary.BigEndian.PutUint32(header[1:5], uint32(len(p)))
-	return append(header, p...)
+	p = append(header, p...)
+	binary.BigEndian.PutUint32(p[1:5], uint32(len(p)))
+	return p
 }
 
-func DecodeCreateNetworkCommand(msg []byte) (*CreateNetworkCommand, error) {
-	var cmd CreateNetworkCommand
+func DecodeDisconnectNetworkCommand(msg []byte) (*DisconnectNetworkCommand, error) {
+	var cmd DisconnectNetworkCommand
+	err := json.Unmarshal(msg, &cmd)
+	return &cmd, err
+}
+func DecodeClientDisconnectNotification(msg []byte) (*ClientDisconnectNotification, error) {
+	var cmd ClientDisconnectNotification
+	err := json.Unmarshal(msg, &cmd)
+	return &cmd, err
+}
+func DecodeClientJoinNotification(msg []byte) (*ClientJoinNotification, error) {
+	var cmd ClientJoinNotification
 	err := json.Unmarshal(msg, &cmd)
 	return &cmd, err
 }
 func DecodeJoinNetworkCommand(msg []byte) (*JoinNetworkCommand, error) {
 	var cmd JoinNetworkCommand
+	err := json.Unmarshal(msg, &cmd)
+	return &cmd, err
+}
+func DecodeCreateNetworkCommand(msg []byte) (*CreateNetworkCommand, error) {
+	var cmd CreateNetworkCommand
+	err := json.Unmarshal(msg, &cmd)
+	return &cmd, err
+}
+func DecodeNetworkJoinNotification(msg []byte) (*NetworkJoinNotification, error) {
+	var cmd NetworkJoinNotification
 	err := json.Unmarshal(msg, &cmd)
 	return &cmd, err
 }
