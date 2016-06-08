9c77e2cf3007107dd7d95414b7bba478fa0ae536
paradoxxl <edee>
2016-06-08 17:45:50 +0200

    some refactoring

--- server/server.go
+++ server/server.go
@@ -22,16 +22,18 @@ func handleClient(conn net.Conn) {
 func (cli *Client) OnCommand(commandType byte, message interface{}) {
 	switch commandType {
 	case msg.CreateNwType:
-		cli.createNetwork(message)
-		cli.joinNetwork(message)
+		data := msg.CreateNetworkCommand{}(message)
+		cli.createNetwork(data.Networkname,data.Networkpass,data.Networkip,data.Peername,data.Peerport)
+		cli.joinNetwork(data.Networkname,data.Networkpass,data.Peername,data.Peerport)
 	case msg.JoinNwType:
-		cli.joinNetwork(message)
+		data := msg.JoinNetworkCommand(message)
+		cli.joinNetwork(data.Networkname,data.Networkpass,data.Peername,data.Peerport)
 	case msg.DisconnectNw:
 		cli.disconnectNetwork()
 	}
 }
-func (cli *Client) createNetwork(cmd msg.CreateNetworkCommand) {
-	_, networkexists := State.m[cmd.Networkname]
+func (cli *Client) createNetwork(Networkname string,Networkpass string,Networkip net.IP,Peername string,Peerport string) {
+	_, networkexists := State.m[Networkname]
 	if networkexists {
 		return
 	}
@@ -39,24 +41,28 @@ func (cli *Client) createNetwork(cmd msg.CreateNetworkCommand) {
 	State.RLock()
 	defer State.RUnlock()
 
-	State.m[cmd.Networkname] = Network{
-		Networkname:    cmd.Networkname,
+	State.m[Networkname] = Network{
+		Networkname:    Networkname,
 		Networkmembers: make(map[string]msg.Peer),
-		Networkpass:    cmd.Networkpass,
-		Networkip:      cmd.Networkip,
+		Networkpass:    Networkpass,
+		Networkip:      Networkip,
 	}
 
-	cli.nw = msg.Network{Networkname: cmd.Networkname}
+	cli.nw = msg.Network{Networkname: Networkname}
 }
 
-func (cli *Client) joinNetwork(cmd msg.JoinNetworkCommand) {
-	_, networkexists := State.m[cmd.Networkname]
+func (cli *Client) joinNetwork(	Networkname string,Networkpass string,Peername string,Peerport string) {
+
+	State.RLock()
+	defer State.RUnlock()
+
+	_, networkexists := State.m[Networkname]
 	if !networkexists {
 		return
 	}
 
 	//get Client IP Address
-	cli.cli.Peeraddress = strings.Split(cli.conn.RemoteAddr().String()[0], ":") + ":" + cmd.Peerport
+	cli.cli.Peeraddress = strings.Split(cli.conn.RemoteAddr().String()[0], ":") + ":" + Peerport
 
 	//check free IP address
 	//TODO: Make it better
@@ -66,8 +72,8 @@ func (cli *Client) joinNetwork(cmd msg.JoinNetworkCommand) {
 		ok = true
 		ip = rand.Intn(253) + 1
 
-		for _, v := range State.m[cmd.Networkname].Networkmembers {
-			cliaddr := v.cli.VirtualAddress[15]
+		for _, v := range State.m[Networkname].Networkmembers {
+			cliaddr := v.cli.VirtualAddress[3]
 			if cliaddr == ip {
 				ok = false
 				return
@@ -75,11 +81,12 @@ func (cli *Client) joinNetwork(cmd msg.JoinNetworkCommand) {
 		}
 	}
 
-	cli.cli.VirtualAddress = State.m[cmd.Networkname].Networkip
-	cli.cli.VirtualAddress[15] = ip
 
-	append(State.m[cmd.Networkname].Networkmembers, cli.cli)
-	cli.nw.Networkname = cmd.Networkname
+	cli.cli.VirtualAddress = State.m[Networkname].Networkip.To4()
+	cli.cli.VirtualAddress[3] = ip
+
+	append(State.m[Networkname].Networkmembers, cli.cli)
+	cli.nw.Networkname = Networkname
 	append(cli.nw.Networkmembers, cli.cli)
 
 	data := msg.EncodeNetworkJoinNotification(msg.NetworkJoinNotification{
@@ -89,7 +96,7 @@ func (cli *Client) joinNetwork(cmd msg.JoinNetworkCommand) {
 
 	//Notify all peers including the new
 
-	for _, v := range State.m[cmd.Networkname].Networkmembers {
+	for _, v := range State.m[Networkname].Networkmembers {
 		n, err := v.conn.Write(data)
 		if err {
 			//TODO: Do something
