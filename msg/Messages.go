14c9f4a96e4e7e651b91e73b72306d2f600f6dff
Raffaele Meyer <rafmeyer91@gmail.com>
2016-06-08 15:32:02 +0200

    Added some stuff

--- msg/Messages.go
+++ msg/Messages.go
@@ -3,12 +3,17 @@ package msg
 import (
 	"net"
 	"errors"
+	"encoding/json"
+	"encoding/binary"
+	"io"
 )
 
+const (
+	Version byte = 10
+)
 
 const (
-	Version = iota
-	CreateNwType
+	CreateNwType byte = iota
 	JoinNwType
 	DisconnectNw
 	NotifyClientDisconect
@@ -42,29 +47,83 @@ type JoinNetworkCommand struct{
 	Peerport string
 }
 
+type MessageHandler interface {
+	OnCommand(commandType byte, message interface{})
+}
 
-func CheckMsg (msg []*byte) (version,cmdType int, err error){
-	version = msg[0]
-
-	length := uint32(msg[2:5])
-	if length < 6 {
-		err = errors.New("Bad Packet length")
-		return
+func ReadMessage(rd io.Reader, handler MessageHandler) (error) {
+	var hdr [6]byte
+	_, err := io.ReadFull(rd, hdr[:])
+	if err != nil {
+		return err
 	}
-	cmdType = msg[5]
-	return
+
+	if hdr[0] != Version {
+		return errors.New("Version mismatch")
+	}
+
+	pktlen := binary.BigEndian.Uint32(hdr[1:5])
+	if pktlen < 6 {
+		return errors.New("Invalid length")
+	}
+	pktlen -= 6
+	if pktlen == 0 {
+		handler.OnCommand(hdr[5], nil)
+		return nil
+	}
+
+	buf := make([]byte, pktlen)
+
+	_, err = io.ReadFull(rd, buf)
+	if err != nil {
+		return err
+	}
+
+	var msg interface{}
+	cmdType := hdr[5]
+	switch cmdType {
+	case CreateNwType:
+		msg, err = DecodeCreateNetworkCommand(buf)
+	case JoinNwType:
+		msg, err = DecodeCreateNetworkCommand(buf)
+	default:
+		err = errors.New("Unsupported command type")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	handler.OnCommand(cmdType, msg)
+	return nil
 }
 
-func EncodeCreateNetworkCommand(data CreateNetworkCommand) []*byte{
-	return
+func EncodeCreateNetworkCommand(data *CreateNetworkCommand) []byte {
+	header := []byte{Version, 0, 0, 0, 0, CreateNwType}
+	p, err := json.Marshal(&data)
+	if err != nil {
+		panic(err)
+	}
+	binary.BigEndian.PutUint32(header[1:5], uint32(len(p)))
+	return append(header, p...)
 }
-func EncodeJoinNetworkCommand(data JoinNetworkCommand) []*byte{
-	return
+func EncodeJoinNetworkCommand(data *JoinNetworkCommand) []byte{
+	header := []byte{Version, 0, 0, 0, 0, JoinNwType}
+	p, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	binary.BigEndian.PutUint32(header[1:5], uint32(len(p)))
+	return append(header, p...)
 }
 
-func DecodeCreateNetworkCommand(msg []*byte) CreateNetworkCommand{
-	return
+func DecodeCreateNetworkCommand(msg []byte) (*CreateNetworkCommand, error) {
+	var cmd CreateNetworkCommand
+	err := json.Unmarshal(msg, &cmd)
+	return &cmd, err
 }
-func DecodeJoinNetworkCommand(msg []*byte) JoinNetworkCommand{
-	return
+func DecodeJoinNetworkCommand(msg []byte) (*JoinNetworkCommand, error) {
+	var cmd JoinNetworkCommand
+	err := json.Unmarshal(msg, &cmd)
+	return &cmd, err
 }
